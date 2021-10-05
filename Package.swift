2fdd7794294dfa594d534c992c41f4d0370e40c0
John Bushnell <jbushnell@kochava.com>
2021-10-05 16:08:26 -0700

    5.0.0

--- Package.swift
+++ Package.swift
@@ -30,13 +30,13 @@ let package = Package(
         .package(
             name: "KochavaCore",
             url: "https://github.com/Kochava/Apple-SwiftPackage-KochavaCore",
-            from: "4.8.0"
+            from: "5.0.0"
         ),
         // .package(name: "KochavaUI", path: "./../Apple-SwiftPackage-KochavaUI")
         .package(
             name: "KochavaUI",
             url: "https://github.com/Kochava/Apple-SwiftPackage-KochavaUI",
-            from: "4.8.0"
+            from: "5.0.0"
         )
     ],
     targets:
